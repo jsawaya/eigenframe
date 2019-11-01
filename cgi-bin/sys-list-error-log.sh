ac221ae72d7d128d2a3ff0ad686f83ade057ba13
John Sawaya <john.sawaya@gmail.com>
2019-11-01 12:18:42 -0400

    eigenscreen

--- cgi-bin/sys-list-error-log.sh
+++ cgi-bin/sys-list-error-log.sh
@@ -6,11 +6,11 @@ echo "Content-type: text/plain; charset=iso-8859-1"
 echo
 
 echo /data/data/com.termux/files/usr/var/log/apache2/error_log:
-tail /data/data/com.termux/files/usr/var/log/apache2/error_log
+tail -n 30 /data/data/com.termux/files/usr/var/log/apache2/error_log
 echo -- eof --
 
 echo /var/log/apache2/error.log:
-tail /var/log/apache2/error.log
+tail -n 30 /var/log/apache2/error.log
 echo -- eof --
 
 
