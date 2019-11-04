748a954e2df9b6fc0de033bae6044d6aad0734de
John Sawaya <john.sawaya@gmail.com>
2019-11-04 10:21:45 -0500

    testing

--- home/githubpull.sh
+++ home/githubpull.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/sh
-
+echo git status:
 git status
+
+echo git checkout master:
 git checkout master
+
+echo git pull:
 git pull
+
+echo git checkout test:
 git checkout test
+
+echo git merge master:
 git merge master
+
+echo git status:
+git status
