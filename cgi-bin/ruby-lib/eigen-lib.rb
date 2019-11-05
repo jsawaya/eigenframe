f24e5e6cfde020a72a865c4c17be33d7131449bf
John Sawaya <photopgp@gmail.com>
2019-11-05 17:03:28 -0500

    refactor

--- cgi-bin/ruby-lib/eigen-lib.rb
+++ cgi-bin/ruby-lib/eigen-lib.rb
@@ -97,131 +97,149 @@ end
 
 
 ##==================================================================================================
-def eigen_directory_listview(dirpath)
-  Dir.chdir dirpath
-  h = directory_hash()
-
-## declare all frames
-  padding = eigen_padding(20)
-  margin = eigen_margin(10)
-  infoLeftIcon = eigen_icon("info.jpg", "left")
-  horzLine = eigen_horizontal_line(2, "#00ff00")
-
-  headerTextView = eigen_text_view(20, "#ffffff", padding, margin, "Directory: #{h['directory']}")
-
-  subdirListView = Hash.new("null")
-  subdirListViewEigenFrame = Hash.new("null")
-  subdirListViewOnClickEigenScreen = Hash.new("null")
-  subdirListViewOnClickJavaScript = Hash.new("null")
-
-  filesListView = Hash.new("null")
-  filesListViewEigenFrame = Hash.new("null")
-  filesListViewOnClickJavaScript = Hash.new("null")
-  filesListViewOnClickPopupTextView = Hash.new("null")
-
-
-## this frame defines the subdirectory listview
-  subdirListView["type"] = "ListView"
-  subdirListView["id"] = 1700
-  subdirListView["layout-width"] = "match_parent"
-  subdirListView["layout-height"] = "wrap_content"
-  subdirListView["layout-weight"] = "1"
-  subdirListView["background-color"] = "#223456"
-  subdirListView["key-list"] = h["subdir"]
-  subdirListView["on-click"] = subdirListViewOnClickEigenScreen
-  subdirListView["eigen-frame"] = subdirListViewEigenFrame
-
-
 ## this frame defines each row of the subdirectory list
+def eigen_list_view_item_layout(size, color, padding, margin, text_script_list)
+  subdirListViewEigenFrame = Hash.new("null")
   subdirListViewEigenFrame["type"] = "TextView"
-  subdirListViewEigenFrame["text-script-list"] =
-    [
-      "eigenFragment.getMapValueInteger(eigenMap, 'position', 0) + ': ' + eigenMap.get('metadata')"
-    ]
-  subdirListViewEigenFrame["text-size"] = "30"
-  subdirListViewEigenFrame["text-color"] = "#ffffff"
   subdirListViewEigenFrame["layout-width"] = "wrap_content"
   subdirListViewEigenFrame["layout-height"] = "wrap_content"
+  subdirListViewEigenFrame["text-size"] = size
+  subdirListViewEigenFrame["text-color"] = color
   subdirListViewEigenFrame["padding"] = padding
   subdirListViewEigenFrame["margin"] = margin
+  subdirListViewEigenFrame["text-script-list"] = text_script_list
+#    [
+#      "eigenFragment.getMapValueInteger(eigenMap, 'position', 0) + ': ' + eigenMap.get('metadata')"
+#    ]
+  subdirListViewEigenFrame
+end
 
-## this defines the EigenScreen executed on-click subdirListView
+
+##==================================================================================================
+## this frame defines how to launch next EigenScreen
+def launch_eigen_screen(size, color, background_color, url_script_list)
+  subdirListViewOnClickEigenScreen = Hash.new("null")
   subdirListViewOnClickEigenScreen["type"] = "EigenScreen"
   subdirListViewOnClickEigenScreen["layout-width"] = "match_parent"
   subdirListViewOnClickEigenScreen["layout-height"] = "wrap_content"
   subdirListViewOnClickEigenScreen["text"] = "EigenFrame"
-  subdirListViewOnClickEigenScreen["text-size"] = "20"
-  subdirListViewOnClickEigenScreen["text-color"] = "#ffffff"
-  subdirListViewOnClickEigenScreen["background-color"] = "#111111"
-  subdirListViewOnClickEigenScreen["url-script-list"] =
-    [
-      "'http://localhost:8080/cgi-bin/sys-directory-listview.rb?dirpath=#{dirpath}' + '/'+ eigenMap.get('option')"
-    ]
+  subdirListViewOnClickEigenScreen["text-size"] = size
+  subdirListViewOnClickEigenScreen["text-color"] = color
+  subdirListViewOnClickEigenScreen["background-color"] = background_color
+  subdirListViewOnClickEigenScreen["url-script-list"] = url_script_list
+#    [
+#      "'http://localhost:8080/cgi-bin/sys-directory-listview.rb?dirpath=#{dirpath}' + '/'+ eigenMap.get('option')"
+#    ]
+  subdirListViewOnClickEigenScreen
+end
 
 
-## this frame defines the file ListView
-  filesListView["type"] = "ListView"
-  filesListView["id"] = 700
-  filesListView["layout-width"] = "match_parent"
-  filesListView["layout-height"] = "wrap_content"
-  filesListView["layout-weight"] = "1"
-  filesListView["background-color"] = "#123456"
-  filesListView["key-list"] = h["files"]
-  filesListView["eigen-frame"] = filesListViewEigenFrame
-#  filesListView["on-click"] = filesListViewOnClickJavaScript
-  filesListView["on-click"] = filesListViewOnClickPopupTextView
+##==================================================================================================
+## this frame defines PopupTextView called via on-click in the file ListView
+def popup_text_view(title_script_list, url_script_list)
+  filesListViewOnClickPopupTextView = Hash.new("null")
+  filesListViewOnClickPopupTextView["type"] = "PopupTextView"
+  filesListViewOnClickPopupTextView["title-script-list"] = title_script_list
+  filesListViewOnClickPopupTextView["url-script-list"] = url_script_list
+  filesListViewOnClickPopupTextView
+end
 
 
-## this frame defines each row of the file list
-  filesListViewEigenFrame["type"] = "TextView"
-  filesListViewEigenFrame["text-script-list"] =
-    [
-      "eigenFragment.getMapValueInteger(eigenMap, 'position', 0) + ': ' + eigenMap.get('metadata')"
-    ]
-  filesListViewEigenFrame["text-size"] = "30"
-  filesListViewEigenFrame["text-color"] = "#ffffff"
-  filesListViewEigenFrame["layout-width"] = "wrap_content"
-  filesListViewEigenFrame["layout-height"] = "wrap_content"
-  filesListViewEigenFrame["padding"] = padding
-  filesListViewEigenFrame["margin"] = margin
-
-
+##==================================================================================================
 ## this frame defines javascript called via on-click in the file ListView
-  filesListViewOnClickJavaScript["type"] = "JavaScript"
-  filesListViewOnClickJavaScript["script-list"] =
+=begin
+  script_list =
     [
       "var pos = eigenFragment.getMapValueInteger(eigenMap, 'position', 0)",
       "var opt = eigenMap.get('option')",
-      "eigenActivity.showToast('ListView file selected: '+ pos + ' - ' + opt)"
+      "eigenActivity.showToast('ListView subdir selected: '+ pos + ' - ' + opt)"
     ]
 
-## this frame defines PopupTextView called via on-click in the file ListView
-  filesListViewOnClickPopupTextView["type"] = "PopupTextView"
-  filesListViewOnClickPopupTextView["title-script-list"] =
+  #filesListView["on-click"] = javascript(script_list)
+=end
+def javascript(script_list)
+  filesListViewOnClickJavaScript = Hash.new("null")
+  filesListViewOnClickJavaScript["type"] = "JavaScript"
+  filesListViewOnClickJavaScript["script-list"] = script_list
+  filesListViewOnClickJavaScript
+end
+
+
+##==================================================================================================
+## this frame subdir_list_view(key_list, itemLayout, subdirListViewOnClickEigenScreen)
+def subdir_list_view(script_list)
+  subdirListView = Hash.new("null")
+  subdirListView["type"] = "ListView"
+  subdirListView["layout-width"] = "match_parent"
+  subdirListView["layout-height"] = "wrap_content"
+  subdirListView["layout-weight"] = "1"
+#  subdirListView["id"] = 1700
+#  subdirListView["background-color"] = "#223456"
+#  subdirListView["key-list"] = h["subdir"]
+  subdirListView["key-list"] = key_list
+  subdirListView["eigen-frame"] = itemLayout
+  subdirListView["on-click"] = subdirListViewOnClickEigenScreen
+  subdirListView
+end
+
+
+##==================================================================================================
+## this frame defines the file ListView
+def file_list_view(key_list, itemLayout, filesListViewOnClickPopupTextView)
+  filesListView = Hash.new("null")
+  filesListView["type"] = "ListView"
+  filesListView["layout-width"] = "match_parent"
+  filesListView["layout-height"] = "wrap_content"
+  filesListView["layout-weight"] = "1"
+#  filesListView["id"] = 700
+#  filesListView["background-color"] = "#123456"
+  filesListView["key-list"] = key_list
+  filesListView["eigen-frame"] = itemLayout
+  filesListView["on-click"] = filesListViewOnClickPopupTextView
+  filesListView
+end
+
+
+##==================================================================================================
+def eigen_directory_listview(dirpath)
+  Dir.chdir dirpath
+  dirHash = directory_hash()
+
+  padding = eigen_padding(20)
+  margin = eigen_margin(10)
+  infoLeftIcon = eigen_icon("info.jpg", "left")
+  horzLine = eigen_horizontal_line(2, "#00ff00")
+  vertLine = eigen_vertical_line(2, "#00ff00")
+
+  headerTextView = eigen_text_view(30, "#ffffff", padding, margin, "List Directory: #{dirHash['directory']}")
+
+  text_script_list = ["eigenFragment.getMapValueInteger(eigenMap, 'position', 0) + ': ' + eigenMap.get('metadata')"]
+  itemLayout = eigen_list_view_item_layout(20, "#ffffff", padding, margin, text_script_list)
+
+  url_script_list = ["'http://localhost:8080/cgi-bin/sys-directory-listview.rb?dirpath=#{dirpath}' + '/'+ eigenMap.get('option')"]
+  subdirListViewOnClickEigenScreen = launch_eigen_screen(20, "#ffffff", "#222222", url_script_list)
+
+  subdirListView = subdir_list_view(dirHash["subdir"], itemLayout, subdirListViewOnClickEigenScreen)
+
+
+  file_item_title_script_list =
     [
       "var title0 = 'File:  #{dirpath}' + '/'+ eigenMap.get('option')",
       "java.lang.System.out.println(title0)",
       "title0"
     ]
-  filesListViewOnClickPopupTextView["url-script-list"] =
+
+  file_item_url_script_list =
     [
       "var url0 = 'http://localhost:8080/cgi-bin/sys-list-termux-file-contents.rb?filepath=#{dirpath}' + '/'+ eigenMap.get('option')",
       "java.lang.System.out.println(url0)",
       "url0"
     ]
 
-=begin
-  subdirListViewOnClickJavaScript["type"] = "JavaScript"
-  subdirListViewOnClickJavaScript["script-list"] =
-    [
-      "var pos = eigenFragment.getMapValueInteger(eigenMap, 'position', 0)",
-      "var opt = eigenMap.get('option')",
-      "eigenActivity.showToast('ListView subdir selected: '+ pos + ' - ' + opt)"
-    ]
-  subdirListView["on-click"] = subdirListViewOnClickJavaScript
-=end
+  filesListViewOnClickPopupTextView = popup_text_view(file_item_title_script_list, file_item_url_script_list)
 
-  vertLine = eigen_vertical_line(2, "#00ff00")
+  filesListView = file_list_view(dirHash["files"], itemLayout, filesListViewOnClickPopupTextView)
+
   layout0 = eigen_horizontal_layout([subdirListView,vertLine,filesListView])
 
   eigenScreenLayout = eigen_vertical_layout([headerTextView,horzLine,layout0])
