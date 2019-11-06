2b4b1d86984927e2b2f231b781cf03398f4d7bd8
John Sawaya <photopgp@gmail.com>
2019-11-06 14:49:47 -0500

    refactor

--- cgi-bin/ruby-lib/eigen-lib.rb
+++ cgi-bin/ruby-lib/eigen-lib.rb
@@ -261,8 +261,15 @@ def eigen_directory_listview(dirpath)
 
   subdirListViewOnClickEigenScreen = launch_eigen_screen(20, "#ffffff", "#222222", url_script_list)
 
-#  image_item_url_script_list = ["file:///storage/emulated/0/Android/data/com.sawaya.eigenframe.full/files/image/eigenframe.png"]
-  image_view = eigen_image_view(url_script_list)
+#  image_url_script_list = ["file:///storage/emulated/0/Android/data/com.sawaya.eigenframe.full/files/image/eigenframe.png"]
+  image_url_script_list =
+    [
+      "var url0 = 'http://localhost:8080/cgi-bin/sys-list-termux-file-contents.rb?filepath=#{dirpath}' + '/'+ eigenMap.get('option')",
+      "java.lang.System.out.println(url0)",
+      "url0"
+    ]
+
+  image_view = eigen_image_view(image_url_script_list)
   fileRowLayout = eigen_horizontal_layout([image_view,fileItemTextView])
 
 
