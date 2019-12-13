6014b2164d4c11e62627638fff16758701227f29
John Sawaya <photopgp@gmail.com>
2019-12-12 20:47:00 -0500

    refactor

--- cgi-bin/ruby-lib/eigen-lib.rb
+++ cgi-bin/ruby-lib/eigen-lib.rb
@@ -85,7 +85,7 @@ def eigen_directory_listview_ssh(dirpath)
     [
       "var filepath0 = '#{dirpath}/' + eigenMap.get('option')",
       "java.lang.System.out.println('js filepath0: '+filepath0)",
-      "var ssh_cmd = 'cd /data/data/com.termux/files/home/git-repos/eigenframe/cgi-bin; ruby list-termux-file-contents ' + filepath0",
+      "var ssh_cmd = 'cd /data/data/com.termux/files/home/git-repos/eigenframe/cgi-bin; ruby list-termux-file-contents.rb ' + filepath0",
       "java.lang.System.out.println('js ssh_cmd: '+ssh_cmd)",
       "ssh_cmd"
     ]
