fbdb9e91975636093f8f2c9b2034c8ce2a0e2d1f
John Sawaya <photopgp@gmail.com>
2020-01-06 19:56:15 -0500

    cleanup

--- cgi-bin/sys-webrick.rb
+++ cgi-bin/sys-webrick.rb
@@ -12,22 +12,7 @@ require '/data/data/com.termux/files/home/git-repos/eigenframe/cgi-bin/ruby-lib/
 
     The following code creates a generic WEBrick server on the local machine on port 1234,
     shuts the server down if the process is interrupted (often done with Ctrl+C).
-    This example lets you call the URL's: "add" and "subtract" and pass through arguments to them
-    Example usage:
-        http://localhost:1234/ (this will show the specified error message)
-        http://localhost:1234/add?a=10&b=10
-        http://localhost:1234/subtract?a=10&b=9
 =end
-
-class MyNormalClass
-    def self.add (a, b)
-        a.to_i + b.to_i
-    end
-
-    def self.subtract (a, b)
-        a.to_i - b.to_i
-    end
-end
 
 class MyServlet < WEBrick::HTTPServlet::AbstractServlet
     def do_GET (request, response)
@@ -41,59 +26,17 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
 
             response.status = 200
             response.content_type = "text/plain"
-            response.body = "Hello World\n"
-
-        elsif request.path == "/listdir"
-            puts "called /listdir"
-
-            dirpath = request.query["dirpath"]
-            puts "dirpath:"
-            p dirpath
-
-            response.status = 200
-            response.content_type = "text/plain"
-
-            if dirpath.empty?
-                dirpath = "/data/data/com.termux/files/home"
-            end
-
-            eigenframe = EigenFrame.new()
-
-            listdir_url = "http://localhost:1234/listdir"
-            list_contents_url = "http://localhost:1234/list_contents"
-            result_json = eigenframe.eigen_directory_listview(dirpath, listdir_url, list_contents_url).to_json
-#             result_json = eigenframe.eigen_directory_listview_http_ruby(dirpath).to_json
-             response.body = result_json.to_s + "\n"
+            response.body = "Hello World"
 
         elsif request.path == "/list_contents"
 
             filepath = request.query["filepath"]
-            puts "filepath:"
-            p filepath
 
-            filetype = request.query["filetype"]
+            #filetype = request.query["filetype"]
 
             response.status = 200
             response.content_type = "text/plain"
             response.body = get_file_as_string(filepath)
-
-        elsif request.query["a"] && request.query["b"]
-            a = request.query["a"]
-            b = request.query["b"]
-            response.status = 200
-            response.content_type = "text/plain"
-
-            result = nil
-            case request.path
-                when "/add"
-                    result = MyNormalClass.add(a, b)
-                when "/subtract"
-                    result = MyNormalClass.subtract(a, b)
-                else
-                    result = "No such method"
-            end
-            
-            response.body = result.to_s + "\n"
 
         else
             response.status = 200
