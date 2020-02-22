b01b1cbfa3803b5df231259ba85bcc426cabf298
John Sawaya <photopgp@gmail.com>
2020-02-22 13:12:23 -0500

    eclipse testing

--- cgi-bin/sys-directory-listview.rb
+++ cgi-bin/sys-directory-listview.rb
@@ -2,8 +2,8 @@
 
 require 'cgi'
 require 'json'
-require './ruby-lib/app-lib'
-require './ruby-lib/eigen-lib'
+require_relative 'ruby-lib/app-lib'
+require_relative 'ruby-lib/eigen-lib'
 
 print_response_header
 
@@ -12,6 +12,6 @@ cgi = CGI.new
 exit 10 if !cgi.has_key?('dirpath')
 dirpath = cgi.params['dirpath'][0]
 
-## this produces json text which is returned by the url request (back to EigenFrame App)
+cmd = "puts EigenFrame.new.eigen_directory_listview_http_cgi(dirpath).to_json.to_s"
 
-puts EigenFrame.new().eigen_directory_listview_http_cgi(dirpath).to_json.to_s
+eval(cmd)
