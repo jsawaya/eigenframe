b40e835fa9ab802e2c0f545e8b2339657e3c4c0d
John Sawaya <photopgp@gmail.com>
2020-02-21 22:41:33 -0500

    eclipse testing

---
+++ cgi-bin/ruby-lib/eigen_padding.rb
@@ -0,0 +1,13 @@
+## define frame for padding
+class EigenFrame
+
+  def eigen_padding(x, y)
+  {
+    left: x,
+    top: y,
+    right: x,
+    bottom: y
+  }
+  end
+
+end
---
+++ cgi-bin/ruby-lib/test/eigen_padding_test.rb
@@ -0,0 +1,26 @@
+# test/eigen_padding_test_test.rb
+require 'test/unit'
+require 'json'
+require_relative '../eigen_padding'
+
+# hashmaps compare properly with keys as symbolize_names
+# 
+
+class Eigen_padding_test < Test::Unit::TestCase
+  def test_hash_with_extentions
+    ef = EigenFrame.new
+    x = ef.eigen_padding(2,3)
+    y = {:left=>2, :right=>2, :top=>3, :bottom=>3}
+    assert x >= y, "#{x.inspect} >= #{y.inspect}"
+  end
+
+  def test_json_with_extentions
+    ef = EigenFrame.new
+    x = ef.eigen_padding(2,3).to_json
+    x1 = JSON.parse(x, {:symbolize_names => true})
+    y = {:bottom=>3, :left=>2, :right=>2, :top=>3}
+    assert x1 >= y, "#{x1.inspect} >= #{y.inspect}"
+  end
+  
+  
+end
