e90976fed862d523d1e8e5abdd4e38dc941fd0f9
John Sawaya <photopgp@gmail.com>
2020-02-24 17:08:34 -0500

    testing

---
+++ cgi-bin/cgi_ruby.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'cgi'
+require 'json'
+require_relative 'ruby-lib/app-lib'
+require_relative 'ruby-lib/eigen-lib'
+
+print_response_header
+
+cgi = CGI.new
+
+p cgi.params.inspect
+
+
+#exit 10 if !cgi.has_key?('dirpath')
+#dirpath = cgi.params['dirpath'][0]
+
+#cmd = "puts EigenFrame.new.eigen_directory_listview_http_cgi(dirpath).to_json.to_s"
+
+#eval(cmd)
