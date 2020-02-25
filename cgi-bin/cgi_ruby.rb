96e59c8cf4a0001d9e0d6f97b481429edec70b07
John Sawaya <photopgp@gmail.com>
2020-02-24 20:39:48 -0500

    testing

--- cgi-bin/cgi_ruby.rb
+++ cgi-bin/cgi_ruby.rb
@@ -2,6 +2,7 @@
 
 require 'cgi'
 require 'json'
+
 require_relative 'ruby-lib/app-lib'
 require_relative 'ruby-lib/eigen-lib'
 
@@ -11,6 +12,18 @@ cgi = CGI.new
 
 puts cgi.params.to_json.to_s
 
+
+cgi.keys.each do |key|
+  puts 'key: '+key
+  value_list = cgi.params[key]
+  value_list.each do |value|
+    puts 'value: '+value
+  end
+#   fields = line.split
+#   keys << fields[-1].strip if fields.size == 4
+ end
+ 
+ 
 #exit 10 if !cgi.has_key?('dirpath')
 #dirpath = cgi.params['dirpath'][0]
 
