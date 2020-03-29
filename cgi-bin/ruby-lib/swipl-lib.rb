12e402e7e36ef227c66185c0b72c9fa7765736e3
John Sawaya <photopgp@gmail.com>
2020-03-29 14:25:17 -0400

    beta branch

---
+++ cgi-bin/ruby-lib/swipl-lib.rb
@@ -0,0 +1,47 @@
+require 'json'
+require 'open3'
+require_relative 'ruby-lib/app-lib'
+require_relative 'ruby-lib/gpg-lib'
+require_relative 'ruby-lib/eigen-lib'
+
+#/cgi-bin/swi-routes.rb?route(nottinghill_gate,maida_vale,X)
+def swipl_test(swipl_path, swipl_goal, outfile)
+
+  #swipl_path = "swipl-lib/routes.pl"
+  puts "swipl_path: ", swipl_path, nil
+
+  #swipl_goal = "route(nottinghill_gate,maida_vale,F)"
+  puts "swipl_goal:", swipl_goal, nil
+
+	cmd = "/data/data/com.termux/files/usr/bin/swipl -s #{swipl_path} -g \"#{swipl_goal}\" -t halt"
+
+  if outfile.to_s.strip.size > 0
+    cmd = cmd << " > #{outfile}"
+  end
+
+  stdin, stdout, stderr, wait_thread = Open3.popen3(cmd)
+#  stdin.puts password
+
+  exit_status = wait_thread.value
+  exit_code = exit_status.exitstatus
+
+  stdout_stringarray = get_io_as_array(stdout)
+  stderr_stringarray = get_io_as_array(stderr)
+
+  stdout.close
+  stderr.close
+
+  {
+    type: "swipl_test",
+    swipl_path: swipl_path,
+    swipl_goal: swipl_goal,
+    kbfile: kbfile,
+    outfile: outfile,
+    command: cmd,
+    exit_code: exit_code,
+    stdout: stdout_stringarray,
+    stderr: stderr_stringarray
+  }
+
+end
+
