4e0bac70a1eb59f8178a29bad68d4f1f73d2e297
John Sawaya <photopgp@gmail.com>
2020-03-29 20:53:33 -0400

    beta branch

--- cgi-bin/ssh_swipl.rb
+++ cgi-bin/ssh_swipl.rb
@@ -11,5 +11,5 @@ exit 10 if ARGV.length == 0
 swipl_path = ARGV[0]
 swipl_goal = ARGV[1]
 
-ruby_string = "ssh_ruby.rb 'swipl_test(\"#{swipl_path}\", \"#{swipl_goal}\")' "
+ruby_string = "swipl_test(\"#{swipl_path}\", \"#{swipl_goal}\")"
 puts eval(ruby_string)
