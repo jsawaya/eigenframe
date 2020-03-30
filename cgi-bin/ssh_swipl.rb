1bbad459248d18493d1403f7de539cd069c984ea
John Sawaya <photopgp@gmail.com>
2020-03-29 20:48:44 -0400

    beta branch

---
+++ cgi-bin/ssh_swipl.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+require 'json'
+require 'open3'
+require_relative 'ruby-lib/app-lib'
+require_relative 'ruby-lib/gpg-lib'
+require_relative 'ruby-lib/eigen-lib'
+require_relative 'ruby-lib/swipl-lib'
+
+exit 10 if ARGV.length == 0
+swipl_path = ARGV[0]
+swipl_goal = ARGV[1]
+
+ruby_string = "ssh_ruby.rb 'swipl_test(\"#{swipl_path}\", \"#{swipl_goal}\")' "
+puts eval(ruby_string)
