8326604c6ed4bb2670b10947ae378e4bd05b06a4
John Sawaya <photopgp@gmail.com>
2020-03-31 16:44:12 -0400

    beta branch

--- cgi-bin/swipl-lib/sudoku.pl
+++ cgi-bin/swipl-lib/sudoku.pl
@@ -6,15 +6,19 @@
 % Example by Markus Triska, taken from the SWI-Prolog manual.
 
 sudoku(Rows) :-
-        length(Rows, 9), maplist(same_length(Rows), Rows),
+        length(Rows, 9),
+        maplist(same_length(Rows), Rows),
         append(Rows, Vs), Vs ins 1..9,
         maplist(all_distinct, Rows),
         transpose(Rows, Columns),
         maplist(all_distinct, Columns),
         Rows = [A,B,C,D,E,F,G,H,I],
-        blocks(A, B, C), blocks(D, E, F), blocks(G, H, I).
+        blocks(A, B, C),
+        blocks(D, E, F),
+        blocks(G, H, I).
 
 blocks([], [], []).
+
 blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
         all_distinct([A,B,C,D,E,F,G,H,I]),
         blocks(Bs1, Bs2, Bs3).
