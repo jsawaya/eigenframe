a6d087bfe6e50461ad7ae4eac7cb63a3551b62f0
John Sawaya <photopgp@gmail.com>
2020-03-30 20:15:30 -0400

    beta branch

--- cgi-bin/swipl-lib/swipl.pl
+++ cgi-bin/swipl-lib/swipl.pl
@@ -1,12 +1,22 @@
 entity(cynthia).
 entity(monica). 
 entity(melissa). 
+entity(dillon).
+entity(brock).
 
 parent(cynthia, monica).
 parent(cynthia, melissa).
+parent(monica, dillon).
+parent(monica, brock).
 
 sibling(X, Y) :-
     parent(Z, X),
     parent(Z, Y),
     X \= Y,
-    write(X),write(' is sibling of '),write(Y),write(' with parent '),write(Z),nl.
+    write('{\"'),write(X),write(' is sibling of '),write(Y),write(' with parent '),write(Z),write('\"}'),nl.
+
+ancestor(X, Y) :-
+    parent(X, Z),
+    parent(Z, Y),
+    X \= Y,
+    write('{\"'),write(X),write(' is sibling of '),write(Y),write(' with parent '),write(Z),write('\"}'),nl.
