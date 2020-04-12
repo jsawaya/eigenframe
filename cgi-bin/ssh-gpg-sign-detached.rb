3991d5d576bdb1ea4ad13b46f7404c492f1032e5
John Sawaya <photopgp@gmail.com>
2020-04-12 12:34:20 -0400

    pgp

--- cgi-bin/ssh-gpg-sign-detached.rb
+++ cgi-bin/ssh-gpg-sign-detached.rb
@@ -6,8 +6,8 @@ require_relative 'ruby-lib/gpg-lib'
 
 exit 10 if ARGV.length == 0 
 
-keyid = ARGV[0]
-password = ARGV[1]
-input_file = ARGV[2]
+input_file = ARGV[0]
+keyid = ARGV[1]
+password = ARGV[2]
 
 puts gpg_sign_detached(keyid, password, input_file).to_json.to_s
