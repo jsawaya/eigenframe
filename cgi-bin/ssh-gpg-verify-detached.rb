6fef81384e7f763e0d445120bf2ef6a84b189760
John Sawaya <photopgp@gmail.com>
2020-04-11 21:30:32 -0400

    pgp

--- cgi-bin/ssh-gpg-verify-detached.rb
+++ cgi-bin/ssh-gpg-verify-detached.rb
@@ -6,6 +6,7 @@ require_relative 'ruby-lib/gpg-lib'
 
 exit 10 if ARGV.length == 0 
 
-keyfile = ARGV[0]
+sig_file = ARGV[0]
+original_file = ARGV[0]
 
-puts gpg_verify(keyfile).to_json.to_s
+puts gpg_verify(sig_file, original_file).to_json.to_s
