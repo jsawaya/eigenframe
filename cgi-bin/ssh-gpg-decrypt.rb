95937324ba4827381e58236f70157314a062074b
John Sawaya <photopgp@gmail.com>
2020-04-12 13:27:26 -0400

    pgp

--- cgi-bin/ssh-gpg-decrypt.rb
+++ cgi-bin/ssh-gpg-decrypt.rb
@@ -1,14 +1,20 @@
 #!/usr/bin/ruby
-# 'ruby ssh-gpg-decrypt.rb ' + encrypted_file + ' ' + output_file + ' \"' + password + '\"' "
+# 'ruby ssh-gpg-decrypt.rb ' + encrypted_file + ' \"' + password + '\"' "
 
 require 'json'
+require 'fileutils'
 require_relative 'ruby-lib/app-lib'
 require_relative 'ruby-lib/gpg-lib'
 
 exit 10 if ARGV.length == 0 
 
 pgpfile = ARGV[0]
-outfile = ARGV[1]
-password = ARGV[2]
+password = ARGV[1]
+
+outfile = encrypted_file[0..(encrypted_file.rindex('.')-1)]
+if File.exist? outfile
+	puts "file already exists: ${outfile}"
+	exit 20
+end
 
 puts gpg_decrypt(password, pgpfile, outfile).to_json.to_s
