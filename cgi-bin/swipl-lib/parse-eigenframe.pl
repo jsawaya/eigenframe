6b5ad81a3b11f98cb493c756def1282edc05d090
jsawaya <john.sawaya@gmail.com>
2020-05-03 22:02:28 -0400

    1.3

--- cgi-bin/swipl-lib/parse-eigenframe.pl
+++ cgi-bin/swipl-lib/parse-eigenframe.pl
@@ -137,83 +137,84 @@ save_json_file(FPath, Data) :-
 
 search_eigenframe_type_test(FPath, Type) :-
 	dyn_json_file_data(FPath, Data), 
-  write(" Try Path: "), 	writeln(FPath),
-	search_eigenframe_type(Data, Type),
-  write(" True Path: "), 	writeln(FPath).
+%  write(" Try Path: "), 	writeln(FPath),
+	search_eigenframe_type(FPath, Data, Type).
+%  write(" True Path: "), 	writeln(FPath).
 
-
+%search_eigenframe_type_test('WebView'),false.
 search_eigenframe_type_test(Type) :-
 	dyn_json_file_data(FPath, Data), 
-  write(" Try Path: "), 	writeln(FPath),
-	search_eigenframe_type(Data, Type),
-  write(" True Path: "), 	writeln(FPath).
+%  write(" Try Path: "), 	writeln(FPath),
+	search_eigenframe_type(FPath, Data, Type).
+%  write(" True Path: "), 	writeln(FPath).
 
 % -------------------------------------
 
-search_eigenframe_list([], _).
-search_eigenframe_list([H|T], Type) :-
-	search_eigenframe_type(H, Type);true,
-  search_eigenframe_list(T, Type).
+search_eigenframe_list(_, [], _).
+search_eigenframe_list(FPath, [H|T], Type) :-
+	search_eigenframe_type(FPath, H, Type);true,
+  search_eigenframe_list(FPath, T, Type).
 
 % -------------------------------------
 
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	X = Data.get(component),
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	X = Data.get(on_click),
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	X = Data.get(on_complete),
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
 % 'ListView' == Type
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	X = Data.get(item_layout), 
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
 % 'AlertDialog' == Type, positive
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	Y = Data.get(positive),
 	X = Y.get(on_complete),
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
 % 'AlertDialog' == Type, negative
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	Y = Data.get(negative),
 	X = Y.get(on_complete),
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
 % 'AlertDialog' == Type, neutral
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	Y = Data.get(neutral),
 	X = Y.get(on_complete),
-	search_eigenframe_type(X, Type).
+	search_eigenframe_type(FPath, X, Type).
 
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	X = Data.get(component_list),
-	search_eigenframe_list(X, Type).
+	search_eigenframe_list(FPath, X, Type).
 
 % 'EigenFrame' == Type
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	X = Data.get(tab_list), 
-	search_eigenframe_list(X, Type).
+	search_eigenframe_list(FPath, X, Type).
 
-%search_eigenframe_type(+Data, +Type)
-search_eigenframe_type(Data, Type) :- 
+%search_eigenframe_type(FPath, +Data, +Type)
+search_eigenframe_type(FPath, Data, Type) :- 
 	Type == Data.get(type),
   write("\n\n= Found eigenframe type: "), writeln(Type),
+  write(" True Path: "), 	writeln(FPath),
 	show_json(Data).
 
 %-----------------------------------------------
